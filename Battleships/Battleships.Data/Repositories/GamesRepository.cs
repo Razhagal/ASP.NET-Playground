f2354585706a3383ceb9baa6f7c20c41019ff195
Hristo Petrov <hpetrovv@gmail.com>
2015-04-12 00:31:44 +0300

    Added repositories

---
+++ Battleships/Battleships.Data/Repositories/GamesRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Battleships.Data.Repositories
+{
+    using System.Linq;
+    using System.Data.Entity;
+
+    using Battleships.Models;
+
+    public class GamesRepository : Repository<Game>
+    {
+        public GamesRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public IQueryable<Game> GetGamesByState(GameState state)
+        {
+            return this.Set.Where(s => s.State == state);
+        }
+    }
+}
