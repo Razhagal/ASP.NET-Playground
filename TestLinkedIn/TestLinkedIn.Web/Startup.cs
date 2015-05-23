3ebb49dc5875a54331d24549200e0ec0a4188746
Hristo Petrov <hpetrovv@gmail.com>
2015-05-23 16:21:39 +0300

    Initial commit. New exercise project - creating a LinkedIn-like project.

---
+++ TestLinkedIn/TestLinkedIn.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestLinkedIn.Web.Startup))]
+namespace TestLinkedIn.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
