db20cdfbba641dec30d3b350ff3c5f218a4a5f91
Hristo Petrov <hpetrovv@gmail.com>
2015-05-23 21:08:30 +0300

    Added repositories and unit of work

---
+++ TestLinkedIn/TestLinkedIn.Data/ITestLinkedInData.cs
@@ -0,0 +1,28 @@
+﻿namespace TestLinkedIn.Data
+{
+    using TestLinkedIn.Data.Repositories;
+    using TestLinkedIn.Models;
+
+    public interface ITestLinkedInData
+    {
+        IRepository<User> Users { get; }
+
+        IRepository<Certification> Certifications { get; }
+
+        IRepository<Discussion> Discussions { get; }
+
+        IRepository<Experience> Experiences { get; }
+
+        IRepository<Group> Groups { get; }
+
+        IRepository<Project> Projects { get; }
+
+        IRepository<Skill> Skills { get; }
+
+        IRepository<Endorcement> Endorcements { get; }
+
+        IRepository<AdministrationLog> AdministrationLogs { get; }
+
+        int SaveChanges();
+    }
+}
